27080b89eee2e80e596d48a023fbb3af5061e92d
matthew <larochelle.matthew@gmail.com>
2023-03-30 14:23:25 -0700

    edit navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,15 +20,28 @@ function Navbar() {
           <li>
             <Link to="/" onClick={()=> setNav(!nav)}>Home</Link>
           </li>
-          <li className={styles.li_dropdown} onClick={()=> setDD1(!dd1)}>
-            About Us <AiOutlineArrowDown className={styles.arrow} />
+          <li className={styles.li_dropdown} 
+              onClick={()=> {
+                setDD1(!dd1);
+                setDD2(false);
+                }}
+              >
+            About Us <AiOutlineArrowDown style={{transform: dd1 ? "rotate(180deg)" : "none"}} className={styles.arrow} />
             {dd1 && <div className={styles.dd1}>
               <Link className={styles.dd_item1} to="about" onClick={()=> setNav(!nav)}>Our Farm</Link>
               <Link className={styles.dd_item} to="horses" onClick={()=> setNav(!nav)}>Our Horses</Link>
             </div>}
           </li>
-          <li className={styles.li_dropdown} onClick={()=> setDD2(!dd2)}>
-            Tours <AiOutlineArrowDown className={styles.arrow} />
+          <li className={styles.li_dropdown} 
+              onClick={()=> {
+                setDD2(!dd2);
+                setDD1(false);
+                }}
+              >
+            Tours <AiOutlineArrowDown 
+                    style={{transform: dd2 ? "rotate(180deg)" : "none"}}  
+                    className={styles.arrow} 
+                    />
             {dd2 && <div className={styles.dd2}>
               <Link className={styles.dd_item1} to="tours" onClick={()=> setNav(!nav)}>Our Tours</Link>
               <Link className={styles.dd_item} to="lights" onClick={()=> setNav(!nav)}>Watkins Regional Park Winter Festival of Lights</Link>
@@ -40,11 +53,8 @@ function Navbar() {
           <li>
             <Link to="contact" onClick={()=> setNav(!nav)}>Contact Us</Link>
           </li>
-          <li>
-          <button type="submit" className={styles.button}>Book Now</button>
-          </li>
-
         </ul>
+        <button type="submit" className={styles.button}>Book Now</button>
       </nav>
       <div onClick={()=> setNav(!nav)} className={styles.mobile_btn}>
         {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}   
